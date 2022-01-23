1b90cea698da6e0326cb4b8a2d07ffc31c1858d0
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-01-23 14:12:33 -0300

    feat: move wait producer send message to gorotina

--- kafka/go/cmd/producer/main.go
+++ kafka/go/cmd/producer/main.go
@@ -10,13 +10,8 @@ func main() {
 	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
 	Publish("mensagem", "teste", producer, nil, deliveryChan)
-	e := <-deliveryChan
-	msg := e.(*kafka.Message)
-	if msg.TopicPartition.Error != nil {
-		fmt.Println("Erro ao enviar")
-	} else {
-		fmt.Println("Mensagem enviada", msg.TopicPartition)
-	}
+	go DeliveryReport(deliveryChan) //async
+	fmt.Println("Cezar Zaleski")
 	producer.Flush(1000)
 }
 
@@ -42,4 +37,20 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 		return err
 	}
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar")
+			} else {
+				fmt.Println("Mensagem enviada", ev.TopicPartition)
+				// anotar no banco de dados que a mensagem foi processada
+			}
+		}
+
+	}
+
 }
