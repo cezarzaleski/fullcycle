32b06c9e4d868f2b7070a4321c6a11e4745b947b
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-01-24 23:40:23 -0300

    feat: create new Product

--- hexagonal/application/product_service_test.go
+++ hexagonal/application/product_service_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestProductService_Get(t *testing.T) {
-
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -22,6 +21,23 @@ func TestProductService_Get(t *testing.T) {
 	}
 
 	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("Product 1", 10)
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
