4319ab79297e3353df6ec94c1bd946867d6d7ef1
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-04-15 00:24:15 -0300

    test: product

---
+++ ddd/tatico/src/entity/product.spec.ts
@@ -0,0 +1,30 @@
+import Product from './product';
+
+
+describe("Product unit test", () => {
+  it("Should throw error when id is empty", () => {
+    expect(() => {
+      new Product("", "Product 1", 100)
+    }).toThrowError("Id is required")
+  })
+  it("Should throw error when name is empty", () => {
+    expect(() => {
+      new Product("123", "", 100)
+    }).toThrowError("Name is required")
+  })
+  it("Should throw error when price is less than zero", () => {
+    expect(() => {
+      new Product("123", "Product1", -1)
+    }).toThrowError("Price must be greater than zero")
+  })
+  it("Should change name", () => {
+      const product = new Product("123", "Product1", 10)
+    product.changeName("Product 2")
+    expect(product.name).toBe("Product 2")
+  })
+  it("Should change price", () => {
+    const product = new Product("123", "Product1", 10)
+    product.changePrice(50)
+    expect(product.price).toBe(50)
+  })
+})
