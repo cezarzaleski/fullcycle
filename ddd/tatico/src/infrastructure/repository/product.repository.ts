c1d3af4a877c50e7c32f2dd6d3c6819dc3016d34
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-04-15 22:33:25 -0300

    feat: implements product.repository.ts

---
+++ ddd/tatico/src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,47 @@
+import ProductInterfaceRepository from '../../domain/repository/product-interface-repository';
+import Product from '../../domain/entity/product';
+import ProductModel from "../db/sequelize/model/product.model";
+
+export default class ProductRepository implements ProductInterfaceRepository {
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price,
+    })
+  }
+
+  async find(id: string): Promise<Product> {
+    const productModel = await ProductModel.findOne({where: {id: id}})
+    return  new Product(
+      productModel.id,
+      productModel.name,
+      productModel.price
+    )
+  }
+
+  async findAll(): Promise<Product[]> {
+    const productsModel = await ProductModel.findAll()
+    return productsModel.map(productModel => {
+      return  new Product(
+        productModel.id,
+        productModel.name,
+        productModel.price
+      )
+    })
+  }
+
+  async update(entity: Product): Promise<void> {
+    await ProductModel.update(
+      {
+        name: entity.name,
+        price: entity.price,
+      },
+      {
+        where: {
+          id: entity.id
+        }
+      }
+    )
+  }
+}
