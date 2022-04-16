12570bd65942dfc071cf9f3afae4f59ca6d86ce8
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-04-15 21:54:59 -0300

    feat: add repository interface

---
+++ ddd/tatico/src/domain/repository/product-interface-repository.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from './repository-interface';
+import Product from '../entity/product';
+
+export default interface ProductInterfaceRepository extends RepositoryInterface<Product>{}
