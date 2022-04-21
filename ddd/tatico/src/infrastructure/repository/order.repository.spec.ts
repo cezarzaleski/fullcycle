2bc777be6cdccaff6bd5b0b30e0386d11444790c
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-04-21 19:09:01 -0300

    feat: implements update order

--- ddd/tatico/src/infrastructure/repository/order.repository.spec.ts
+++ ddd/tatico/src/infrastructure/repository/order.repository.spec.ts
@@ -11,11 +11,25 @@ import Product from "../../domain/entity/product";
 import OrderItem from "../../domain/entity/order_item";
 import Order from "../../domain/entity/order";
 import OrderRepository from "../repository/order.repository";
+import CustomerInterfaceRepository from "../../domain/repository/customer-interface-repository";
+import ProductInterfaceRepository from "../../domain/repository/product-interface-repository";
+import OrderInterfaceRepository from "../../domain/repository/order-interface-repository";
 
 describe("Order repository test", () => {
   let sequelize: Sequelize;
+  let customerRepository: CustomerInterfaceRepository;
+  let productRepository: ProductInterfaceRepository;
+  let subject: OrderInterfaceRepository;
+  let customer: Customer;
+  let product: Product;
 
   beforeEach(async () => {
+    customerRepository = new CustomerRepository();
+    productRepository = new ProductRepository();
+    subject = new OrderRepository();
+    customer = new Customer("123", "Customer 1");
+    customer.Address = new Address(1, "Street 1", "City 1", "Zipcode 1");
+    product = new Product("p1", "Product 1", 10)
     sequelize = new Sequelize({
       dialect: "sqlite",
       storage: ":memory:",
@@ -32,17 +46,11 @@ describe("Order repository test", () => {
   });
 
   it("should create a new order", async () => {
-    const customerRepository = new CustomerRepository();
-    const productRepository = new ProductRepository();
-    const orderRepository = new OrderRepository();
-    const customer = new Customer("123", "Customer 1");
-    const product = new Product("p1", "Product 1", 10)
-    customer.Address = new Address(1, "Street 1", "City 1", "Zipcode 1");
     const orderItem = new OrderItem("i1", product.name, product.price, product.id, 2)
     const order = new Order("1", customer.id, [orderItem])
     await productRepository.create(product);
     await customerRepository.create(customer);
-    await orderRepository.create(order)
+    await subject.create(order)
 
     const orderModel = await OrderModel.findOne({where: {id: order.id}, include: ["items"]});
     expect(orderModel.toJSON()).toStrictEqual({
@@ -63,43 +71,48 @@ describe("Order repository test", () => {
   });
 
   it('should find a order', async () => {
-    const customerRepository = new CustomerRepository();
-    const productRepository = new ProductRepository();
-    const orderRepository = new OrderRepository();
-    const customer = new Customer("123", "Customer 1");
-    const product = new Product("p1", "Product 1", 10)
-    customer.Address = new Address(1, "Street 1", "City 1", "Zipcode 1");
     const orderItem = new OrderItem("i1", product.name, product.price, product.id, 2)
     const order = new Order("1", customer.id, [orderItem])
     await productRepository.create(product);
     await customerRepository.create(customer);
-    await orderRepository.create(order)
+    await subject.create(order)
 
-    const orderResult = await orderRepository.find(order.id);
+    const orderResult = await subject.find(order.id);
 
     expect(order).toStrictEqual(orderResult);
   });
 
   it('should find all orders', async () => {
-    const customerRepository = new CustomerRepository();
-    const productRepository = new ProductRepository();
-    const orderRepository = new OrderRepository();
-    const customer = new Customer("123", "Customer 1");
-    const product = new Product("p1", "Product 1", 10)
     customer.Address = new Address(1, "Street 1", "City 1", "Zipcode 1");
     const orderItem1 = new OrderItem("i1", product.name, product.price, product.id, 2)
     const order1 = new Order("1", customer.id, [orderItem1])
     await productRepository.create(product);
     await customerRepository.create(customer);
-    await orderRepository.create(order1)
+    await subject.create(order1)
     const orderItem2 = new OrderItem("i2", product.name, product.price, product.id, 2)
     const order2 = new Order("2", customer.id, [orderItem2])
-    await orderRepository.create(order2)
+    await subject.create(order2)
 
-    const orders = await orderRepository.findAll();
+    const orders = await subject.findAll();
 
     expect(orders).toHaveLength(2);
     expect(orders).toContainEqual(order1);
     expect(orders).toContainEqual(order2);
+  });
+
+  it('should update a order', async () => {
+    const orderItem = new OrderItem("i1", product.name, product.price, product.id, 2)
+    const orderItemUpdate = new OrderItem("i2", product.name, product.price, product.id, 4)
+    const order = new Order("1", customer.id, [orderItem])
+    await productRepository.create(product);
+    await customerRepository.create(customer);
+    await subject.create(order)
+    const orderUpdate = new Order("1", customer.id, [orderItemUpdate])
+
+    await subject.update(orderUpdate)
+
+    const orderResult = await subject.find(order.id);
+
+    expect(orderUpdate).toStrictEqual(orderResult);
   });
 })
--- ddd/tatico/src/infrastructure/repository/order.repository.ts
+++ ddd/tatico/src/infrastructure/repository/order.repository.ts
@@ -53,5 +53,25 @@ export default class OrderRepository implements OrderInterfaceRepository {
   }
 
   async update(entity: Order): Promise<void> {
+    const sequelize = OrderModel.sequelize;
+    await sequelize.transaction(async (t) => {
+      await OrderItemModel.destroy({
+        where: {order_id: entity.id},
+        transaction: t,
+      });
+      const items = entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }));
+      await OrderItemModel.bulkCreate(items, {transaction: t});
+      await OrderModel.update(
+        {total: entity.total()},
+        {where: {id: entity.id}, transaction: t}
+      );
+    });
   }
 }
